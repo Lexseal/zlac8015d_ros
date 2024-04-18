d2421c1c6c30ae5aa1219e5d0d80a137ea2bed00
Xinsong Lin <zhongjiezhex12@me.com>
2024-04-18 12:04:57 -0700

    it's moving with teleop

--- zlac8015d_ros/driver.py
+++ zlac8015d_ros/driver.py
@@ -1,55 +1,69 @@
-import rospy
-from geometry_msgs.msg import Twist
-
-from zlac8015d import ZLAC8015D
+from time import time
 
 import numpy as np
+import rclpy
+from geometry_msgs.msg import Twist
+from rclpy.node import Node
+from zlac8015d import ZLAC8015D
 
-class ZLAC8015D:
+
+class Driver(Node):
     def __init__(self):
-        rospy.init_node('ZLAC8015D', anonymous=True)
-        self.cmd_vel_sub = rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
-        self.last_cmd_vel_time = rospy.get_time()
-        self.drive_timer = rospy.Timer(rospy.Duration(1.0/60.0), self.drive_callback)  # 60Hz timer
+        super().__init__("driver")
 
-        self.left_right_rpm = np.zeros(2)
-        self.radius = 0.126
-        self.wheel_diamaeter = 0.1
-        self.wheel_cirumference = np.pi * self.wheel_diamaeter
+        self.cmd_vel_sub = self.create_subscription(
+            Twist, "/cmd_vel", self.cmd_vel_callback, 10
+        )
 
-        motors = ZLAC8015D.Controller(port="/dev/ttyUSB0")
+        self.cmd_vel_sub  # prevent unused variable warning
 
-        motors.disable_motor()
+        self.last_cmd_vel_time = time()
+        self.drive_timer = self.create_timer(1 / 60, self.drive_callback)
 
-        motors.set_accel_time(1000,1000)
-        motors.set_decel_time(1000,1000)
+        self.left_right_cmd_speed = np.zeros(2)
+        self.robot_radius = 0.165
 
-        # if MODE == 1:
-		# 	print("Set relative position control")
-		# elif MODE == 2:
-		# 	print("Set absolute position control")
-		# elif MODE == 3:
-		# 	print("Set speed rpm control")
-        motors.set_mode(3)
-        motors.enable_motor()
+        self.motors = ZLAC8015D.Controller()
+
+        self.motors.disable_motor()
+
+        self.motors.set_accel_time(1000, 1000)
+        self.motors.set_decel_time(1000, 1000)
+
+        # if MODE == 1: print("Set relative position control")
+        # elif MODE == 2: print("Set absolute position control")
+        # elif MODE == 3: print("Set speed rpm control")
+        self.motors.set_mode(3)
+        self.motors.enable_motor()
 
     def cmd_vel_callback(self, msg):
         # Update the last received command velocity time
-        self.last_cmd_vel_time = rospy.get_time()
+        self.last_cmd_vel_time = time()
 
         # Convert twist to left and right wheel speeds
         linear = msg.linear.x
         angular = msg.angular.z
-        left_right_speed = linear + np.array([-angular * self.radius, angular * self.radius])
-        self.left_right_rpm = left_right_speed / self.wheel_cirumference * 60
+        self.left_right_cmd_speed = linear + np.array(
+            [-angular * self.robot_radius, angular * self.robot_radius]
+        )
+        # one wheel is backwards
+        self.left_right_cmd_speed[0] = -self.left_right_cmd_speed[0]
 
     def drive_callback(self):
         # If we haven't received a command velocity message in the last second, stop the motors
-        if rospy.get_time() - self.last_cmd_vel_time > 1.0:
-            self.motors.set_rpm(0,0)
+        if time() - self.last_cmd_vel_time > 1:
+            self.motors.set_speed(0, 0)
         else:
-            self.motors.set_rpm(*self.left_right_rpm)
+            self.motors.set_speed(*self.left_right_cmd_speed)
 
-if __name__ == '__main__':
-    node = ZLAC8015D()
-    rospy.spin()
+
+def main(args=None):
+    rclpy.init(args=args)
+    driver = Driver()
+    rclpy.spin(driver)
+    driver.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
