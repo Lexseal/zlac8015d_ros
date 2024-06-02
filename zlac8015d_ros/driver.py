a64d4b07107e565355f8cefdedd889183b503ce1
Xinsong Lin <zhongjiezhex12@me.com>
2024-06-02 16:07:16 -0700

    better calculation of odom and publishing imu based on odom

--- zlac8015d_ros/driver.py
+++ zlac8015d_ros/driver.py
@@ -10,7 +10,7 @@ from geometry_msgs.msg import (
     Twist,
     TwistWithCovariance,
 )
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, Imu
 from rclpy.node import Node
 from tf2_ros import TransformBroadcaster, StaticTransformBroadcaster
 from zlac8015d import ZLAC8015D
@@ -29,6 +29,7 @@ class Driver(Node):
         self.cmd_vel_sub  # prevent unused variable warning
 
         self.odom_pub = self.create_publisher(Odometry, "/odom", 10)
+        self.imu_pub = self.create_publisher(Imu, "/imu", 10)
 
         self.last_cmd_vel_time = self.get_clock().now()
         self.drive_timer = self.create_timer(1 / 60, self.drive_callback)
@@ -63,8 +64,14 @@ class Driver(Node):
         self.odom_msg.twist = self.twist_with_covariance
         self.odom_msg.pose = self.pose_with_covariance
 
-        self.odom_calc_timer = self.create_timer(1 / 60, self.odom_calc_callback)
-        self.tf_pub_timer = self.create_timer(1 / 30, self.tf_pub_callback)
+        self.imu_msg = Imu()
+        self.imu_msg.header.frame_id = "imu_link"
+        self.imu_msg.orientation_covariance = np.eye(3).flatten().tolist()
+        self.imu_msg.angular_velocity_covariance = np.eye(3).flatten().tolist()
+        self.imu_msg.linear_acceleration_covariance = np.eye(3).flatten().tolist()
+
+        self.odom_calc_timer = self.create_timer(1 / 100, self.odom_calc_callback)
+        self.tf_pub_timer = self.create_timer(1 / 50, self.tf_pub_callback)
 
         self.transform_broadcaster = TransformBroadcaster(self)
         self.static_broadcaster = StaticTransformBroadcaster(self)
@@ -78,6 +85,7 @@ class Driver(Node):
         self.tf_footprint.transform.translation.z = -0.03
 
         self.cur_x, self.cur_y, self.cur_z = 0, 0, 0
+        self.last_speed = 0
 
     def cmd_vel_callback(self, msg):
         # Update the last received command velocity time
@@ -102,22 +110,29 @@ class Driver(Node):
     def odom_calc_callback(self):
         cur_time = self.get_clock().now()
         self.odom_msg.header.stamp = cur_time.to_msg()
+        dt = (cur_time - self.last_odom_time).nanoseconds / 1e9
+        self.last_odom_time = cur_time
+        
         left_speed, right_speed = self.motors.get_linear_velocities()
         left_speed *= self.motor_polarity[0]
         right_speed *= self.motor_polarity[1]
-        avg_speed = (left_speed + right_speed) / 2
-        self.twist_with_covariance.twist.linear.x = avg_speed
-        omega = (right_speed - left_speed) / (2 * self.robot_radius)
-        self.twist_with_covariance.twist.angular.z = omega
-
-        dt = (cur_time - self.last_odom_time).nanoseconds / 1e9
-        dx = avg_speed * np.cos(self.cur_z) * dt
-        dy = avg_speed * np.sin(self.cur_z) * dt
-        self.cur_x += dx
-        self.cur_y += dy
-        self.cur_z += omega * dt
-
-        self.last_odom_time = cur_time
+        
+        R = self.robot_radius * (left_speed + right_speed) / (right_speed - left_speed)
+        omega = (right_speed - left_speed) / (2*self.robot_radius)
+        ICC = self.cur_x - R*np.sin(self.cur_z), self.cur_y + R*np.cos(self.cur_z)
+        Mat = np.array([[np.cos(omega*dt), -np.sin(omega*dt), 0],
+                        [np.sin(omega*dt), np.cos(omega*dt), 0],
+                        [0, 0, 1]])
+        new_pose = Mat @ np.array([self.cur_x - ICC[0], self.cur_y - ICC[1], self.cur_z]) + np.array([ICC[0], ICC[1], omega*dt])
+        self.cur_x, self.cur_y, self.cur_z = new_pose
+        
+        self.imu_msg.header.stamp = cur_time.to_msg()
+        # angular velocity only in z direction
+        self.imu_msg.angular_velocity.z = omega
+        cur_speed = (right_speed + left_speed) / 2
+        self.imu_msg.linear_acceleration.x = (cur_speed - self.last_speed) / dt
+        self.last_speed = cur_speed
+        self.imu_pub.publish(self.imu_msg)
 
     def tf_pub_callback(self):
         self.pose_with_covariance.pose.position.x = self.cur_x
